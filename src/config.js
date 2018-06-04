eb4abad4fff6e5a683e80bfd636d632d6f900977
Ex-iT <vic.toothpick@gmail.com>
2018-06-04 23:16:54 +0200

    Update config for build

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
 	api: {
-		channelLogoSrc: 'http://tvgidsassets.nl/img/channels/140x140/%s.png',
-		programsUrl: 'http://json.tvgids.nl/v3/programs/?day=0',
-		programUrl: 'http://json.tvgids.nl/v3/program/%s/',
+		channelLogoSrc: 'https://tvgidsassets.nl/img/channels/140x140/%s.png',
+		programsUrl: 'https://json.tvgids.nl/v3/programs/?day=0',
+		programUrl: 'https://json.tvgids.nl/v3/program/%s/',
 	},
 	channels: [1, 2, 3, 4, 31, 46, 92, 36, 460, 37, 34, 91, 7, 8, 440],
 	genre_id: 6
