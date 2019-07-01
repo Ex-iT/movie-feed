d75b338073d9cfdd05cea41e8bc57a16b33de5df
Ex-iT <vic.toothpick@gmail.com>
2019-07-01 15:37:44 +0200

    Update deps

--- scripts/build-post.js
+++ scripts/build-post.js
@@ -3,6 +3,6 @@ const del = require('del');
 const path = require('path');
 
 (async () => {
-    const deletedPaths = await del([path.join('build', 'service-worker.js')]);
-    console.log(`[+] Removed ${deletedPaths.join(',')}`);
+	const deletedPaths = await del([path.join('build', 'service-worker.js')]);
+	console.log(`[+] Removed ${deletedPaths.join(',')}`);
 })();
