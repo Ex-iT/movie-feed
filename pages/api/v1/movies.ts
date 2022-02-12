c4efc5e077f9f376d06a41237e77d873076494b0
Ex-iT <vic.toothpick@gmail.com>
2022-02-12 13:26:10 +0100

    Fixed start of day date/time

--- pages/api/v1/movies.ts
+++ pages/api/v1/movies.ts
@@ -7,6 +7,7 @@ import {
   DEEP_LINK,
   EMPTY_IMG,
   MOVIES_URI,
+  NEXT_STARTS_AT,
 } from '../../../config';
 import fetchData from '../../../lib/fetchData';
 import formatDate from '../../../lib/formatDate';
@@ -51,18 +52,22 @@ const filterChannels = (channels: Array<Prog>) => {
 };
 
 const enrichData = (channelData: Array<Prog>) => {
+  const ONE_DAY = 24 * 3600;
   return channelData
     .map(movie => {
       const { ch_id, ps, pe } = movie;
       const now = getEpoch();
-      const start = parseInt(ps, 10);
+      let start = parseInt(ps, 10);
       let end = parseInt(pe, 10);
 
-      // @TODO: end time on the next day (after 12 midnight)
-      // is on the same day (date) as start time.
-      // This is a crude fix for that until the API is fixed.
-      if (start > end) {
-        end = end + 24 * 3600; // Add 24 hours
+      // Adjust start/end time to make the next
+      // day start at `NEXT_STARTS_AT` at night
+      if (getHours(start) <= NEXT_STARTS_AT) {
+        start = start + ONE_DAY;
+        end = end + ONE_DAY;
+      }
+      if (getHours(end) <= 0) {
+        end = end + ONE_DAY;
       }
 
       return {
@@ -75,26 +80,24 @@ const enrichData = (channelData: Array<Prog>) => {
         progress: getProgress(now, start, end),
         deep_link: getDeepLinkUrl(movie.title),
         day: formatDate(start),
-        // @TODO: Remove `pe`.
-        // Overwriting the `pe` here to
-        // return the 'fixed' end time
+        // Overwriting the `ps` and `pe` here to
+        // return the 'fixed' start and end time
+        ps: `${start}`,
         pe: `${end}`,
       };
     })
     .sort((a, z) => parseInt(a.ch_id, 10) - parseInt(z.ch_id, 10));
 };
 
-const getChannelLogo = (id: string) => {
-  return id ? CHANNEL_LOGO_SRC.replace(/%s/g, id) : EMPTY_IMG;
-};
+const getChannelLogo = (id: string) =>
+  id ? CHANNEL_LOGO_SRC.replace(/%s/g, id) : EMPTY_IMG;
 
-const getChannelLabel = (id: number) => {
-  return CHANNELS[id] || '';
-};
+const getChannelLabel = (id: number) => CHANNELS[id] || '';
 
-function getDeepLinkUrl(title: string) {
-  return `${DEEP_LINK}/${slugify(title, {
+const getDeepLinkUrl = (title: string) =>
+  `${DEEP_LINK}/${slugify(title, {
     decamelize: false,
     customReplacements: [['&', '']],
   })}`;
-}
+
+const getHours = (timestamp: number) => new Date(timestamp * 1000).getHours();
