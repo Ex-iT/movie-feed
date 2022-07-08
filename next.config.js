c15bec52f6c4bd7453528fa1e89754f2050acbb4
Ex-iT <vic.toothpick@gmail.com>
2022-07-08 09:36:27 +0200

    Updated deps and used next preact plugin

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
-module.exports = {
+const withPreact = require('next-plugin-preact');
+
+module.exports = withPreact({
   poweredByHeader: false,
   swcMinify: true,
   reactStrictMode: true,
   images: {
     domains: ['tvgidsassets.nl'],
   },
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-
-    return config;
-  },
-};
+});
